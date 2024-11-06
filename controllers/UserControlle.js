8dd593e2502cf755f3fbfa1667cbace03a3c70f2
JBdevCristian <jbcristian78@gmail.com>
2024-11-05 21:04:07 -0300

    Criando metodo de busca com knex e incriptando senha com bcrypt

--- controllers/UserControlle.js
+++ controllers/UserControlle.js
@@ -12,13 +12,22 @@ class UserController {
         if(email == undefined || email == "") {
             res.status(400);
             res.json({err: "O e-mail é invalido"})
+            return;
+        }
+
+        var emailExists = await user.findEmail(email);
+
+        if(emailExists) {
+            res.status(406);
+            res.json({err: "E-mail já cadastrado"})
+            return
         }
 
         await user.new(name, email, password);
 
         res.status(200);
         console.log("Usuario adicionado com sucesso")
-        res.send("Pegando users da tabela")
+        res.send("Usuario adicionado com sucesso")
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -4,9 +4,26 @@ var bcrypt = require("bcrypt");
 class User{
     async new(name, email, password) {
         try {
-            await knex.insert({name, email, password, role: 0}).table("users");
+            var hash = await bcrypt.hash(password, 10);
+            await knex.insert({name, email, password: hash, role: 0}).table("users");
         } catch(err) {
             console.log(err)
+        }
+    }
+
+    async findEmail(email) {
+        try {
+            
+            var result = await knex.select("*").from("users").where({email: email});
+            if(result.length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch(err) {
+            console.log(err);
+            return false;
         }
     }
 }
